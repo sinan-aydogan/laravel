ca39601d97860c366fa1c48e72bcbbaf272aa911
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-12-25 19:40:28 +0300

    Quantity sütununu ve stok ilişkilerini ekler

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name'];
+
+    /*Warehouses*/
+    public function warehouses():BelongsToMany{
+        return $this->belongsToMany(Warehouse::class)->withPivot('quantity');
+    }
 }
