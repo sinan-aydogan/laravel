ded8d761de224288a1a039d6484d63655e5c7ec4
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2023-01-14 00:07:43 +0300

    Düzeltme: Çift bildirim hatasını düzeltir

--- resources/js/Layouts/VisitorLayout.vue
+++ resources/js/Layouts/VisitorLayout.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onUpdated, onMounted } from "vue";
+import { ref, onUpdated, onMounted, watch } from "vue";
 import { Head, Link, usePage } from "@inertiajs/inertia-vue3";
 import { useI18n } from "vue-i18n";
 import Langs from "../Langs/langs";
@@ -52,9 +52,15 @@ onMounted(() => {
     updateLang();
 })
 onUpdated(() => {
-    addNotification();
+    // addNotification();
     updateLang();
 })
+
+/*Tracking Flash Notification*/
+watch(()=>usePage().props.value.flash.message,()=>{
+    addNotification()
+})
+
 const changeLang = (lang) => {
     locale.value = lang;
     showLangs.value = false;
@@ -138,67 +144,3 @@ const changeLang = (lang) => {
         <t-notification />
     </teleport>
 </template>
-
-<style scoped>
-.bg-gray-100 {
-    background-color: #f7fafc;
-    background-color: rgba(247, 250, 252, var(--tw-bg-opacity));
-}
-
-.border-gray-200 {
-    border-color: #edf2f7;
-    border-color: rgba(237, 242, 247, var(--tw-border-opacity));
-}
-
-.text-gray-400 {
-    color: #cbd5e0;
-    color: rgba(203, 213, 224, var(--tw-text-opacity));
-}
-
-.text-gray-500 {
-    color: #a0aec0;
-    color: rgba(160, 174, 192, var(--tw-text-opacity));
-}
-
-.text-gray-600 {
-    color: #718096;
-    color: rgba(113, 128, 150, var(--tw-text-opacity));
-}
-
-.text-gray-700 {
-    color: #4a5568;
-    color: rgba(74, 85, 104, var(--tw-text-opacity));
-}
-
-.text-gray-900 {
-    color: #1a202c;
-    color: rgba(26, 32, 44, var(--tw-text-opacity));
-}
-
-@media (prefers-color-scheme: dark) {
-    .dark\:bg-gray-800 {
-        background-color: #2d3748;
-        background-color: rgba(45, 55, 72, var(--tw-bg-opacity));
-    }
-
-    .dark\:bg-gray-900 {
-        background-color: #1a202c;
-        background-color: rgba(26, 32, 44, var(--tw-bg-opacity));
-    }
-
-    .dark\:border-gray-700 {
-        border-color: #4a5568;
-        border-color: rgba(74, 85, 104, var(--tw-border-opacity));
-    }
-
-    .dark\:text-white {
-        color: #fff;
-        color: rgba(255, 255, 255, var(--tw-text-opacity));
-    }
-
-    .dark\:text-gray-400 {
-        color: #cbd5e0;
-        color: rgba(203, 213, 224, var(--tw-text-opacity));
-    }
-}
-</style>
