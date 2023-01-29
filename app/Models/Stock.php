4d04919d09ea2de4f02f6351ccee6fa805e6aa1b
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2023-01-29 22:45:05 +0300

    Stok Projesi-Stok Kartı için ara model oluşturma

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -4,27 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Stock extends Model
 {
     use HasFactory;
 
     protected $table = "product_warehouse";
+    protected $fillable = ['quantity'];
 
     /*Product*/
-    public function product():BelongsTo{
-        return $this->belongsTo(Product::class);
+    public function product(){
+        return $this->hasOne(Product::class, 'id', 'product_id');
     }
 
     /*Warehouse*/
-    public function warehouse():BelongsTo{
-        return $this->belongsTo(Warehouse::class);
-    }
-
-    /*Transactions*/
-    public function transactions():HasMany{
-        return $this->hasMany(Transaction::class);
+    public function warehouse(){
+        return $this->hasOne(Warehouse::class, 'id', 'warehouse_id');
     }
 }
--- routes/routes.php
+++ routes/routes.php
@@ -4,4 +4,28 @@ Route::middleware('auth:sanctum')->group(function (){
     Route::resource('warehouse', \App\Http\Controllers\WarehouseController::class);
     Route::resource('product', \App\Http\Controllers\ProductController::class);
     Route::resource('transaction', \App\Http\Controllers\TransactionController::class);
+
+    /*Temp Route*/
+    Route::get('stock/{product}/{warehouse}/{quantity}/{type}', function ($product, $warehouse, $quantity, $type){
+        $stockCard = \App\Models\Stock::where(['product_id' => $product, 'warehouse_id' => $warehouse])->first();
+
+        /*Update Stock*/
+        if($type=='incoming'){
+            /*Add*/
+            $stockCard->update([
+                'quantity' => $stockCard->quantity + $quantity
+            ]);
+        }else{
+            /*Extraction*/
+            if($stockCard->quantity > $quantity){
+                $stockCard->update([
+                    'quantity' => $stockCard->quantity - $quantity
+                ]);
+            }else{
+                return 'Yetersiz stok';
+            }
+
+        }
+        return $stockCard;
+    });
 });
